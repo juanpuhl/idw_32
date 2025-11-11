44e6dbc89fcf5f4fe7e1cf37c744260440eb50b0
Valentina <valentelinanieto@gmail.com>
2025-11-10 22:59:54 -0300

    Actualizacion

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const rol = sessionStorage.getItem('rolUsuario'); 
+    const rol = sessionStorage.getItem('rolUsuario');
+    const adminLinks = document.querySelectorAll('.admin-only');
 
-    if (rol !== 'admin') {
-        const adminLinks = document.querySelectorAll('.admin-only');
+    // Si no hay rol o no es admin, ocultar los enlaces de admin
+    if (!rol || rol !== 'admin') {
         adminLinks.forEach(link => link.style.display = 'none');
+    } else {
+        adminLinks.forEach(link => link.style.display = 'block');
     }
 });
